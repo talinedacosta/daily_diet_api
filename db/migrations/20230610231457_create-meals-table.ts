fc8f43ec8ab499339de879aec3d70e2beb4fca9a
Taline <talinedacosta@gmail.com>
2023-06-10 20:26:01 -0300

    set up database with knex and add tsconfig file

---
+++ db/migrations/20230610231457_create-meals-table.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable('meals', (table) => {
+        table.uuid('id').primary();
+        table.uuid('user_id').notNullable().index();
+        table.text('title').notNullable();
+        table.text('description');
+        table.boolean('isDiet').notNullable();
+        table.timestamp('eaten_at').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable('meals');
+}
+
---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import { knex as setupKnex, Knex } from 'knex';
+import { env } from './env';
+
+const databaseURL = env.DATABASE_CLIENT === 'sqlite' ? {
+    filename: env.DATABASE_URL
+} : env.DATABASE_URL
+
+export const config: Knex.Config = {
+    client: env.DATABASE_CLIENT,
+    connection: databaseURL,
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './db/migrations'
+    }
+}
+
+export const knex = setupKnex(config);
